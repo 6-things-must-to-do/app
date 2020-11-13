a70b014fc1b203d4f08845f4e940af895925c999
Jung <jungeun.lak@gmail.com>
2020-11-13 16:27:12 +0900

    style(dashboard): StyleSheet -> Styled Component

--- src/screens/Dashboard/Presenter.tsx
+++ src/screens/Dashboard/Presenter.tsx
@@ -1,10 +1,11 @@
 import TaskList from '@/containers/TaskList';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
 import * as Progress from 'react-native-progress';
 import {VictoryBar, VictoryLabel} from 'victory-native';
 import StyledText from '@/components/StyledText';
-import {MAIN_BLACK, MAIN_GREEN, SUB_WHITE, SUB_GRAY} from '@/constants/colors';
+import useTheme from '@/hooks/useTheme';
+import styled from 'styled-components/native';
+import StyledView from '@/components/StyledView';
 
 interface Props {
   data: Array<{x: number; y: number}>;
@@ -13,71 +14,70 @@ interface Props {
 export default (props: Props) => {
   const {data} = props;
 
+  const theme = useTheme();
+
   return (
-    <View style={styles.temp}>
-      <View style={styles.graphView}>
+    <Wrapper>
+      <GraphView>
         <VictoryBar
           data={data}
           labels={({datum}) => datum.x}
           labelComponent={<VictoryLabel dy={0} />}
           style={{
             data: {
-              fill: ({datum}) => (datum.y === 1 ? MAIN_GREEN : SUB_GRAY)
+              fill: ({datum}) => (datum.y === 1 ? theme.tint : theme.outfocus)
             },
             labels: {
-              fill: SUB_WHITE
+              fill: theme.text.default
             }
           }}
         />
-      </View>
-      <View style={styles.dateView}>
-        <StyledText color="default" style={styles.bigtext}>
-          11월 12일 (목)
-        </StyledText>
-        <StyledText color="default" style={styles.textCenter}>
-          4/5 (80%)
-        </StyledText>
-      </View>
-      <View style={styles.progressView}>
+      </GraphView>
+      <DateView>
+        <BigCenterText color="default">11월 12일 (목)</BigCenterText>
+        <CenterText color="default">4/5 (80%)</CenterText>
+      </DateView>
+      <ProgressView>
         <Progress.Bar
           progress={4 / 6}
-          color={MAIN_GREEN}
+          color={theme.tint}
           width={300}
           height={2}
         />
-      </View>
-      <View style={styles.listVeiw}>
+      </ProgressView>
+      <ListView>
         <TaskList />
-      </View>
-    </View>
+      </ListView>
+    </Wrapper>
   );
 };
 
-const styles = StyleSheet.create({
-  temp: {
-    flex: 1,
-    backgroundColor: MAIN_BLACK
-  },
-  graphView: {
-    flex: 1.4
-  },
-  dateView: {
-    flex: 0.35
-    // justifyContent: 'center'
-  },
-  progressView: {
-    flex: 0.1,
-    alignItems: 'center'
-  },
-  listVeiw: {
-    flex: 2
-  },
-  textCenter: {
-    textAlign: 'center'
-  },
-  bigtext: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 20
-  }
-});
+const Wrapper = styled(StyledView)`
+  flex: 1;
+`;
+
+const GraphView = styled.View`
+  flex: 1.4;
+`;
+
+const DateView = styled.View`
+  flex: 0.35;
+`;
+
+const ProgressView = styled.View`
+  flex: 0.1;
+  align-items: center;
+`;
+
+const ListView = styled.View`
+  flex: 2;
+`;
+
+const CenterText = styled(StyledText)`
+  text-align: center;
+`;
+
+const BigCenterText = styled(CenterText)`
+font-weight: bold,
+font-size: 20
+`;
